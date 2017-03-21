1121ad8f914785325fff2c3a6fe933b87fd205c7
Alex Ruzhinskiy <alexruzhinskiy@gmail.com>
2017-03-21 16:30:26 +0200

    finished 2 tasks

--- app/index.js
+++ app/index.js
@@ -1,2 +1,20 @@
-import First from './source/first-task';
-import Second from './source/second-task';
+import ObjectCreator from './source/first-task';
+import StringReverser from './source/second-task';
+
+const str = 'Wake up, Neo. The Matrix has you... Follow the white rabbit. Knock, knock, Neo.';
+
+const obj = new ObjectCreator(str, true);
+const resultOne = obj.getObject();
+console.log(resultOne);
+
+const obj2 = new ObjectCreator();
+obj2.convertedString = str;
+const resultTwo = obj2.getObject();
+console.log(resultTwo);
+
+//-----------------------------------------------//
+
+const str2 = 'foobar';
+
+const reversedStr = StringReverser(str2);
+console.log(reversedStr);
--- app/source/first-task.js
+++ app/source/first-task.js
@@ -1,2 +1,58 @@
-console.log('first');
-export default 1;
+// 1) https://gist.github.com/foxvision/e39d8cac1d339d1966b5bb7e8aaabac0
+
+export default class ObjectCreator {
+    constructor(convertedString, ignoreCapitalLetter = false) {
+        this.__convertedString = convertedString;
+        this.ignoreCapitalLetter = ignoreCapitalLetter;
+        this.__objectFromArray = {};
+        this.__convertedStringChanged = true;
+    }
+
+    get convertedString() {
+        return this.__convertedString;
+    }
+
+    set convertedString(convertedString) {
+        if (!convertedString || typeof(convertedString ) !== 'string') {
+            throw new Error('please setup correct String')
+        }
+        this.__convertedStringChanged = true;
+        this.__convertedString = convertedString;
+        return this;
+    }
+
+    getObject() {
+        if (!this.convertedString || typeof(this.convertedString ) !== 'string') {
+            throw new Error('no String for getting Object!')
+        }
+
+        if (!this.__convertedStringChanged) {
+            return this.__objectFromArray;
+        }
+
+        const cleanArr = this.__convertedString.split(' ').map(str => this.getCleanString(str));
+
+        this.__convertedStringChanged = false;
+        return this.convertToObjectFromArray(cleanArr);
+    }
+
+    getCleanString(str) {
+        return str.replace(new RegExp(/[\u0021-\u002e]/, 'g'), '');
+    }
+
+    convertToObjectFromArray(arr) {
+
+        if (this.ignoreCapitalLetter) {
+            arr = arr.map(string => string.toLowerCase());
+        }
+
+        arr.forEach(string => {
+            if (!this.__objectFromArray [string]) {
+                this.__objectFromArray [string] = 1;
+                return;
+            }
+            this.__objectFromArray [string] += 1;
+        });
+        return this.__objectFromArray;
+    }
+}
