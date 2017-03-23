c70c8c06d99c1d1ceb3ce663c18b38af34c089ca
Alex Ruzhinskiy <alexruzhinskiy@gmail.com>
2017-03-23 16:36:48 +0200

    added bootsrap + loaders + styles

--- app/index.js
+++ app/index.js
@@ -1,1 +1,4 @@
-console.log('hello world')
+import './assets/assets.module';
+
+
+var test = 25;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const vendor = require('./config/vendor.json');
 
 
@@ -27,7 +27,9 @@ module.exports = {
             multiStep: true
         }),
         new webpack.ProvidePlugin({
-            'jQuery': 'jquery',
+            jQuery: 'jquery',
+            $: 'jquery',
+            jquery: 'jquery'
         }),
         new CopyWebpackPlugin([
                 {
@@ -50,7 +52,24 @@ module.exports = {
 
     module: {
         loaders: [
-            {test: /\.js$/, exclude: [/node_modules/, /bower_components/], loader: "babel-loader"}
+            {test: /\.js$/, exclude: [/node_modules/, /bower_components/], loader: "babel-loader"},
+            { test: /\.scss/, exclude: /node_modules/, loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap&includePaths[]=node_modules/compass-mixins/lib'},
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: "style-loader",
+                        use: "css-loader"
+                    })
+                }
+            ]
+        },
+        plugins: [
+            new ExtractTextPlugin("styles.css"),
         ]
     },
     devServer: {
